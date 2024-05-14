dbb8717f571c4a3fe04f4dad5228845c6ed89ead
Ian Harry <iwharry@googlemail.com>
2024-05-14 22:08:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -#-
+# Copyright (2024) Ian Harry (ian.harry@port.ac.uk)
+
+"""Build configuration for pn_lal_threaded
+"""
+
+import os
+
+from setuptools import setup, Extension
+
+import numpy
+
+from Cython.Build import cythonize
+
+__author__ = "Ian Harry <ian.harry@port.ac.uk>"
+
+
+# define cython options
+cython_compile_args = [
+    "-O3",
+    "-w",
+    "-ffast-math",
+    "-ffinite-math-only",
+    "-std=c99",
+]
+cython_directives = {
+    "embedsignature": True,
+    "language_level": 3,
+}
+
+# enable coverage for cython
+if int(os.getenv("CYTHON_LINETRACE", "0")):
+    cython_directives["linetrace"] = True
+    cython_compile_args.append("-DCYTHON_TRACE")
+
+# define compiled extensions
+exts = [
+    Extension(
+        "sbank.overlap_cpu",
+        ["sbank/overlap_cpu.pyx"],
+        include_dirs=[numpy.get_include()],
+        language="c",
+        libraries=["lal"],
+        extra_compile_args=cython_compile_args,
+        extra_link_args=[],
+    ),
+]
+
+# -- build the thing
+# this function only manually specifies things that aren't
+# supported by setup.cfg (as of setuptools-30.3.0)
+setup(
+    ext_modules=cythonize(exts, compiler_directives=cython_directives),
+)
